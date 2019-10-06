1ad816ac01a7df6575a8cdc0a2ac2b1f90642e08
Louis Blin <louis-blin@hotmail.fr>
2019-10-06 18:30:40 +0100

    Switch to powerlevel 10k

--- install_scripts/oh-my-zsh.zsh
+++ install_scripts/oh-my-zsh.zsh
@@ -4,6 +4,6 @@ INSTALL_DIR=~/.oh-my-zsh
 
 mkdir -p $(dirname $INSTALL_DIR)
 git clone https://github.com/robbyrussell/oh-my-zsh.git "$INSTALL_DIR" || true
-git clone [200~https://github.com/romkatv/powerlevel10k "$INSTALL_DIR"/custom/themes/powerlevel10k || true
+git clone https://github.com/romkatv/powerlevel10k "$INSTALL_DIR"/custom/themes/powerlevel10k || true
 git -C "$INSTALL_DIR" fetch
 git -C "$INSTALL_DIR" reset --hard origin/master
